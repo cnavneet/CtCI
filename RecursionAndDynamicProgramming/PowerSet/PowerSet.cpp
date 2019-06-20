71f5768efd494571cc6fca0ba682e333a17f124b
cnavneet <cnavneet58@gmail.com>
2019-06-20 22:54:49 +0530

    Adding code for printing all the subsets of a set

---
+++ RecursionAndDynamicProgramming/PowerSet/PowerSet.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void PrintPowerSet(int arr[], int n)
+{
+    for(int i = 0;i<n;++i)
+    {
+        for(int j = i;j<n;++j)
+        {
+            for(int k = i;k<=j;++k)
+            {
+                cout<<arr[k]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0;i<n;++i)
+    {
+        cin>>arr[i];
+    }
+
+    PrintPowerSet(arr, n);
+    return 0;
+}
